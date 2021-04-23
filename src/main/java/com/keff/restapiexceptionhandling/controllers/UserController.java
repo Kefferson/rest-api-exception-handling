8d021227712b0d2ba14fc1a5037368e7c69310a8
Kefferson Costa <keffersonsc@gmail.com>
2021-04-22 23:25:44 -0300

    Added new endpoints to the UserController

--- src/main/java/com/keff/restapiexceptionhandling/controllers/UserController.java
+++ src/main/java/com/keff/restapiexceptionhandling/controllers/UserController.java
@@ -1,17 +1,16 @@
 package com.keff.restapiexceptionhandling.controllers;
 
 import com.keff.restapiexceptionhandling.dtos.UserRequestDTO;
+import com.keff.restapiexceptionhandling.dtos.UserResponseDTO;
 import com.keff.restapiexceptionhandling.entities.User;
 import com.keff.restapiexceptionhandling.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
@@ -26,5 +25,25 @@ public class UserController {
         User user = userService.saveUser(dto);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(user.toResponse());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getUserInfo(@PathVariable Long id) {
+
+        User user = userService.findById(id);
+
+        return ResponseEntity.ok(user.toResponse());
+    }
+
+    @GetMapping
+    public ResponseEntity<?> listUsers() {
+
+        List<UserResponseDTO> userList = userService.findAll();
+
+        if (userList.isEmpty()) {
+            return ResponseEntity.ok("No users were found.");
+        }
+
+        return ResponseEntity.ok(userService.findAll());
     }
 }
--- src/main/java/com/keff/restapiexceptionhandling/services/UserService.java
+++ src/main/java/com/keff/restapiexceptionhandling/services/UserService.java
@@ -1,10 +1,16 @@
 package com.keff.restapiexceptionhandling.services;
 
 import com.keff.restapiexceptionhandling.dtos.UserRequestDTO;
+import com.keff.restapiexceptionhandling.dtos.UserResponseDTO;
 import com.keff.restapiexceptionhandling.entities.User;
 import com.keff.restapiexceptionhandling.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService {
@@ -15,5 +21,24 @@ public class UserService {
     public User saveUser(UserRequestDTO userRequestDTO) {
 
         return userRepository.save(userRequestDTO.toEntity());
+    }
+
+    public User findById(Long id) {
+
+        Optional<User> possibleUser = userRepository.findById(id);
+
+        if (possibleUser.isEmpty()) {
+            throw new NoSuchElementException("User not found.");
+        }
+
+        return possibleUser.get();
+    }
+
+    public List<UserResponseDTO> findAll() {
+
+        return userRepository.findAll()
+                .stream()
+                .map(User::toResponse)
+                .collect(Collectors.toList());
     }
 }
